647f69be0cc141176a3856167859d1b37d178906
Your Name <you@example.com>
2021-04-25 23:27:45 -0400

    added color to bottom

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,16 +49,23 @@ $(".card .list-group").sortable({
   tolerance: "pointer", 
   helper: "clone", 
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate:function(event){
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over:function(event){
-    console.log("over", this);
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out:function(event){
-    console.log("put", event.target);
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active")
+  },
+  stop: function(event) {
+    $(this).removeClass("dropover");
   },
   update: function(event) {
     var tempArr = [];
@@ -84,9 +91,7 @@ $(".card .list-group").sortable({
     tasks[arrName] = tempArr;
     saveTasks();
   },
-  stop: function(event) {
-    $(this).removeClass("dropover");
-  }
+
 });
 
 $("#modalDueDate").datepicker({
@@ -207,7 +212,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -246,6 +251,12 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
